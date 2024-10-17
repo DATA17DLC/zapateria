7f4b2048c4b4330aea190fe2f9dfe436016d39b9
Jesus Tafur <gamerarow123@gmail.com>
2024-10-16 23:39:51 -0500

    comentario

---
+++ backend/autenticar.js
@@ -0,0 +1,31 @@
+// login.js
+
+// Capturar el formulario de login
+const form = document.getElementById('loginForm');
+
+// Añadir el evento de submit
+form.addEventListener('submit', function (e) {
+    e.preventDefault();  // Evitar recarga de la página
+
+    // Obtener los valores de los campos del formulario
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Hacer la solicitud POST con Axios
+    axios.post('http://127.0.0.1:8000/login_users', {
+        email: email,
+        password: password
+    })
+    .then(function (response) {
+        console.log(response.data);  // Si el login es exitoso
+        alert("Login successful!");
+        // Redirigir a otra página si es necesario
+        // window.location = "dashboard.html"; 
+    })
+    .catch(function (error) {
+        console.error(error.response.data);  // Si ocurre un error
+        alert("Login failed: " + error.response.data.detail);
+    });
+    // abrir ventana dashboard si los datos son correctos
+    window.location = "dashboard.html";
+});
